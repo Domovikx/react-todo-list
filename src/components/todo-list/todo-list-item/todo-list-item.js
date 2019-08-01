741423e8cf3ce8c734055d3c1e9b6c3410400db7
Ilya Ivanouski <domovikx@gmail.com>
2019-08-01 14:36:16 +0300

    chore: add Material UI style for button

--- src/components/todo-list/todo-list-item/todo-list-item.js
+++ src/components/todo-list/todo-list-item/todo-list-item.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    button: {
+        margin: theme.spacing(1),
+    },
+    input: {
+        display: 'none',
+    },
+}));
+
+
 function TodoListItem() {
+    const classes = useStyles();
+
     return (
         <li>
             <p>1</p>
-            <button>important</button>
-            <button>del</button>
+            <Button variant="contained"
+                color=""
+                className={classes.button}>
+                important
+            </Button>
+            <Button variant="contained"
+                color=""
+                className={classes.button}>
+                del
+            </Button>
+
         </li>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+import 'typeface-roboto';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
