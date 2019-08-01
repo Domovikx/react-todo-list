c816a584518c0d5932577ec5574fff292b0c7727
Ilya Ivanouski <domovikx@gmail.com>
2019-08-01 11:09:26 +0300

    feat: add first information

---
+++ src/components/app/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      <div>information</div>
+      <ul>
+        <li>
+          <p>1</p>
+          <button>important</button>
+          <button>del</button>
+        </li>
+      </ul>
+      <div>other</div>      
+    </div>
+  );
+}
+
+export default App;
