c783d2ca40ebf78f252fb234747ae7e3bf4c63b5
Ilya Ivanouski <domovikx@gmail.com>
2019-08-01 11:22:11 +0300

    feat: making first structures

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,18 +3,43 @@ import React from 'react';
 function App() {
   return (
     <div className="app">
-      <h1>Todo List</h1>
-      <div>information</div>
-      <ul>
-        <li>
-          <p>1</p>
-          <button>important</button>
-          <button>del</button>
-        </li>
-      </ul>
-      <div>other</div>      
+      <AppBaseHeader />
+      <AppIinformation />
+      <TodoList />
+      <TodoListItem />
     </div>
   );
 }
+
+function AppBaseHeader() {
+  return (
+    <h1>Todo List</h1>
+  );
+}
+
+function AppIinformation() {
+  return (
+    <div>information</div>
+  );
+}
+
+function TodoList() {
+  return (
+    <ul>
+      <li>
+        <p>1</p>
+        <button>important</button>
+        <button>del</button>
+      </li>
+    </ul>
+  );
+}
+
+function TodoListItem() {
+  return (
+    <div>other</div>
+  );
+}
+
 
 export default App;
