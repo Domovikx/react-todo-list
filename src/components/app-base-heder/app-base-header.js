58711450f52ac7f1907ed4be27ba55cd135d782f
Ilya Ivanouski <domovikx@gmail.com>
2019-08-02 11:43:46 +0300

    refactor: useStyles

--- src/components/app-base-heder/app-base-header.js
+++ src/components/app-base-heder/app-base-header.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import { Typography, Button, AppBar, Toolbar, Link, makeStyles } from '@material-ui/core';
+import { Typography, AppBar, Toolbar, Link, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-	// '@global': {
-	// 	body: {
-	// 		backgroundColor: theme.palette.common.white,
-	// 	},
-	// 	ul: {
-	// 		margin: 0,
-	// 		padding: 0,
-	// 	},
-	// 	li: {
-	// 		listStyle: 'none',
-	// 	},
-	// },
 	appBar: {
 		borderBottom: `1px solid ${theme.palette.divider}`,
 	},
@@ -25,29 +13,7 @@ const useStyles = makeStyles(theme => ({
 	},
 	link: {
 		margin: theme.spacing(1, 1.5),
-	},
-	heroContent: {
-		padding: theme.spacing(8, 0, 6),
-	},
-	cardHeader: {
-		backgroundColor: theme.palette.grey[200],
-	},
-	cardPricing: {
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'baseline',
-		marginBottom: theme.spacing(2),
-	},
-	// footer: {
-	// 	borderTop: `1px solid ${theme.palette.divider}`,
-	// 	marginTop: theme.spacing(8),
-	// 	paddingTop: theme.spacing(3),
-	// 	paddingBottom: theme.spacing(3),
-	// 	[theme.breakpoints.up('sm')]: {
-	// 		paddingTop: theme.spacing(6),
-	// 		paddingBottom: theme.spacing(6),
-	// 	},
-	// },
+	}
 }));
 
 function AppBaseHeader() {
