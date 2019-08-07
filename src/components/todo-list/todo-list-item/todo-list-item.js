934aaa6c9f5d54d24af02f3fa8c3639f7882f13c
Ilya Ivanouski <domovikx@gmail.com>
2019-08-07 17:01:54 +0300

    refactor: edit styles

--- src/components/todo-list/todo-list-item/todo-list-item.js
+++ src/components/todo-list/todo-list-item/todo-list-item.js
@@ -1,41 +1,56 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 import ButtonDel from './button-del';
 import ButtonDone from './button-done';
 import ButtonImportant from './button-important';
 
-function TodoListItem({ content, done = false, important = false, ...props }) {
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    margin: theme.spacing(1),
+    backgroundColor: 'whitesmoke',    
+  },
+  typography: {
+    color: 'black',
+  },
+}));
 
+function TodoListItem({ content, done = false, important = false, ...props }) {
+  const classes = useStyles();
   return (
     <Grid
       container
       justify="center"
       alignItems="center"
     >
-      <Grid >
-        <Typography>
-          <pre style={{ fontFamily: 'inherit' }}>
+      <Paper className={classes.paper}>
+        <Grid >
+          <Typography className={classes.typography}>
             {content}
-          </pre>
-        </Typography>
-      </Grid>
+          </Typography>
+        </Grid>
 
-      <Grid >
-        <ButtonImportant
-          important={important}
-          onImportant={props.onImportant}
-        />
-        <ButtonDone
-          done={done}
-          onDone={props.onDone}
-        />
-        <ButtonDel
-          onDelited={props.onDelited}
-        />
-      </Grid>
+        <Grid >
+          <ButtonImportant
+            important={important}
+            onImportant={props.onImportant}
+          />
+          <ButtonDone
+            done={done}
+            onDone={props.onDone}
+          />
+          <ButtonDel
+            onDelited={props.onDelited}
+          />
 
+        </Grid>
+      </Paper>
     </Grid>
   );
 }
