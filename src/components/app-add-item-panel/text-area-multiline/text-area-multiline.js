69c42262315c2f16ca864e79f7e4fbab5d82e617
Ilya Ivanouski <domovikx@gmail.com>
2019-08-07 09:11:02 +0300

    chore: add e.target.value

--- src/components/app-add-item-panel/text-area-multiline/text-area-multiline.js
+++ src/components/app-add-item-panel/text-area-multiline/text-area-multiline.js
@@ -10,16 +10,22 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+
+function onInputChange(event) {
+  console.log(event.target.value);
+}
+
 export default function TextAreaMultiline() {
   const classes = useStyles();
   return (
     <TextField
       id="standard-textarea"
-      label="With placeholder multiline"
-      placeholder="Placeholder"
+      label="Please enter your text"
+      placeholder="..."
       multiline
       className={classes.textField}
       margin="normal"
+      onChange={onInputChange}
     />
   );
 }
